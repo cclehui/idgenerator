4634c3634bb71d015b9c8a8e74d47086fc9c2c91
cclehui <god_chen@sina.com>
2017-07-28 19:31:08 +0800

    conccrunt map

--- test/test.go
+++ test/test.go
@@ -9,9 +9,12 @@ import(
     "idGenerator/config"
     "idGenerator/persistent"
     "github.com/gin-gonic/gin"
+    "github.com/cmap"
 )
 
-var idWorkerMap = make(map[int]*idGenerator.IdWorker)
+//每个业务对应一个 key 全局唯一
+//var idWorkerMap = make(map[int]*idGenerator.IdWorker)
+var idWorkerMap = cmap.New();
 
 func main() {
 
@@ -29,7 +32,6 @@ func main() {
     fmt.Printf("%#v\n", db);
     return;
 
-
     r := gin.Default()
 
     r.GET("/ping", func(c *gin.Context) {
@@ -40,17 +42,27 @@ func main() {
 
     // Get ID
     r.GET("/worker/:id", func(c *gin.Context) {
-        id, _ := strconv.Atoi(c.Params.ByName("id"))
-        value, ok := idWorkerMap[id]
-        if ok {
-            nid, _ := value.NextId()
+        workerId := c.Params.ByName("id");
+        currentWorker, ok := idWorkerMap.Get(workerId);
+        value, typeOk := currentWorker.(idGenerator.IdWorker);
+
+        if ok && typeOk {
+            //获取下一个递增id
+            nid, _ := value.NextId();
+
             c.JSON(200, gin.H{"id": nid})
+
         } else {
-            iw, err := idGenerator.NewIdWorker(int64(id))
+
+            id, _ := strconv.Atoi(workerId);
+
+            idWorker, err := idGenerator.NewIdWorker(int64(id))
             if err == nil {
-                nid, _ := iw.NextId()
-                idWorkerMap[id] = iw
+                nid, _ := idWorker.NextId();
+                idWorkerMap.Set(workerId, idWorker);
+
                 c.JSON(200, gin.H{"id": nid})
+
             } else {
                 fmt.Println(err)
             }
